43d4580f0b1b9cbe290062212d03f70a0065da4b
David Eberly <deberly@geometrictools.com>
2025-01-19 12:24:04 -0800

    Modified file version to current date.
    I forgot to replace yyyy.mm.dd by the actual date of posting.

--- GTL/Graphics/SceneGraph/Hierarchy/ViewVolumeNode.cpp
+++ GTL/Graphics/SceneGraph/Hierarchy/ViewVolumeNode.cpp
@@ -3,7 +3,7 @@
 // Copyright (c) 2025 Geometric Tools LLC
 // Distributed under the Boost Software License, Version 1.0
 // https://www.boost.org/LICENSE_1_0.txt
-// File Version: yyyy.mm.dd
+// File Version: 2025.01.19
 
 #include <GTLGraphicsPCH.h>
 #include <GTL/Graphics/SceneGraph/Hierarchy/ViewVolumeNode.h>
