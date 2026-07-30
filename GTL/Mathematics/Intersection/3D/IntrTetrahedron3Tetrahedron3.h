b9cea811691694396f513cd938fe06e7d8186de3
David Eberly <deberly@geometrictools.com>
2026-07-30 16:12:01 -0700

    Update IntrTetrahedron3Tetrahedron3.h
    Defect found by Claude Sonnet5/Fable5. The edge-edge double loop had P1=tetra0.v[edge1Indices[0]]. The wrong tetrahedron is accessed. It
    should be tetra1.

--- GTL/Mathematics/Intersection/3D/IntrTetrahedron3Tetrahedron3.h
+++ GTL/Mathematics/Intersection/3D/IntrTetrahedron3Tetrahedron3.h
@@ -3,7 +3,7 @@
 // Copyright (c) 2025 Geometric Tools LLC
 // Distributed under the Boost Software License, Version 1.0
 // https://www.boost.org/LICENSE_1_0.txt
-// File Version: 0.0.2025.10.12
+// File Version: 0.0.2026.07.30
 
 #pragma once
 
@@ -123,7 +123,7 @@ namespace gtl
                 for (std::size_t i1 = 0; i1 < 6; ++i1)
                 {
                     auto const& edge1Indices = Tetrahedron3<T>::GetEdgeIndices(i1);
-                    Vector3<T> P1 = tetra0.v[edge1Indices[0]];
+                    Vector3<T> P1 = tetra1.v[edge1Indices[0]];
                     Vector3<T> E1 = tetra1.v[edge1Indices[1]] - P1;
 
                     if (std::fabs(Dot(E0, E1)) < cutoff)
