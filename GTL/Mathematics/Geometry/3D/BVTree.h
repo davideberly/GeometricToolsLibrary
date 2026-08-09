b0105337f3d68bbfc8ca2968eb2849d7555be095
David Eberly <deberly@geometrictools.com>
2026-08-09 10:48:11 -0700

    Update BVTree.h
    I had fixed a bug in the bounding volume tree code for GTE; see the August 1, 2026 update notes. Unfortunately, I had not fixed the bug in the GTL code. A regression was introduced when backported the GTL code to GTE. I fixed this.

--- GTL/Mathematics/Geometry/3D/BVTree.h
+++ GTL/Mathematics/Geometry/3D/BVTree.h
@@ -3,7 +3,7 @@
 // Copyright (c) 2025 Geometric Tools LLC
 // Distributed under the Boost Software License, Version 1.0
 // https://www.boost.org/LICENSE_1_0.txt
-// File Version: 0.0.2026.08.08
+// File Version: 0.0.2026.08.09
 
 #pragma once
 
@@ -226,7 +226,7 @@ namespace gtl
             nodeIndices.clear();
 
             auto linearBoundaryVolumeQuery = this->mLinearBoundingVolumeQuery[queryType];
-            std::vector<std::size_t> indexStack(2 * this->mHeight);
+            std::vector<std::size_t> indexStack(2 * this->mHeight + 1);
             std::size_t top = 0;
             indexStack[0] = 0;
             while (top != std::numeric_limits<std::size_t>::max())
