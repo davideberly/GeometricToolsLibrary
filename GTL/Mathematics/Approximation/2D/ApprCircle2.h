e76392733f44a5ce174a4cd655e3949bfccbaced
David Eberly <deberly@geometrictools.com>
2026-08-09 11:48:41 -0700

    Fixed return value of approximator.
    Defect found by Claude Sonnet5/Fable5. FitUsingSquaredLengths has a loop that terminates if the number of iterations exceeds maxIterations specified by the caller. The old return value is ++iteration, which is maxIterations+1 on non-convergence. I now return iteration.

--- GTL/Mathematics/Approximation/2D/ApprCircle2.h
+++ GTL/Mathematics/Approximation/2D/ApprCircle2.h
@@ -3,7 +3,7 @@
 // Copyright (c) 2025 Geometric Tools LLC
 // Distributed under the Boost Software License, Version 1.0
 // https://www.boost.org/LICENSE_1_0.txt
-// File Version: 0.0.2025.01.28
+// File Version: 0.0.2026.08.09
 
 #pragma once
 
@@ -152,7 +152,7 @@ namespace gtl
                 }
             }
 
-            return ++iteration;
+            return iteration;
         }
 
     private:
